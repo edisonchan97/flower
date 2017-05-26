1754a61075c0ab92ba39aa129059d35b78f91bb2
edisonchan97 <edisonzhuozhou@163.com>
2017-05-26 17:02:01 +0800

    flower

---
+++ js/cart-vue.js
@@ -0,0 +1,16 @@
+new Vue({
+    el:".flower-cart",
+    data:{
+       totalprice:0,
+        price:1900,
+        qty:1
+
+    },
+        computed:{
+            totalprice:function(){
+                //var totalprice =0;
+                //totalprice = price*qty
+            }
+        }
+
+})
