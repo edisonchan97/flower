c7b2328233798f8f9214a4c8f0faa2dc46286f9f
edisonchan97 <edisonzhuozhou@163.com>
2017-06-02 09:28:32 +0800

    flower

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 /**
  * Created by 41563 on 2017/5/31.
  */
+function AddFavorite(title, url) {
+    try {
+        window.external.addFavorite(url, title);
+    }
+    catch (e) {
+        try {
+            window.sidebar.addPanel(title, url, "");
+        }
+        catch (e) {
+            alert("sorry,fail collet\n\ncollect Ctrl+D");
+        }
+    }
+}
